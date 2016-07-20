492be3430355274dbe54f9d129fd1fda614e87a0
Allen Bierbaum <abierbaum@gmail.com>
2016-07-20 15:03:49 -0500

    Redo the in repo packages.

--- in_repo/README.md
+++ in_repo/README.md
@@ -13,3 +13,20 @@ Build the modules
 Show the files created by the build (there might not be any), add then to .gitignore
 > git status
 
+
+## Packages Changed (removed, updated, etc)
+
+Download modules without building them
+> rm -rf node_modules
+> npm install --ignore-scripts 
+> git status #   see removed modules
+> git add . && git commit -a   # Check in modules
+
+## Now rebuild the packages and find build files with git status
+
+Build the modules
+> npm rebuild
+
+Show the files created by the build (there might not be any), add then to .gitignore
+> git status
+
--- in_repo/node_modules/bootstrap-sass/test/node_mincer_test.rb
+++ in_repo/node_modules/bootstrap-sass/test/node_mincer_test.rb
@@ -22,14 +22,15 @@ class NodeMincerTest < Minitest::Test
 
   def setup
     tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    command = "node manifest.js #{tmp_dir}"
     success = Dir.chdir DUMMY_PATH do
       silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+        system(command)
       end
     end
     assert success, 'Node.js Mincer compilation failed'
     manifest = JSON.parse(File.read("#{tmp_dir}/manifest.json"))
-    css_name = manifest['assets']['application.css']
+    css_name = manifest["assets"]["application.css"]
     @css = File.read("#{tmp_dir}/#{css_name}")
   end
 end
